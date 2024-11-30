b7d5c5268d1972a168d876c67a25689b806ff47a
omprakash-singh <omprakashsingh11009@gmail.com>
2024-11-30 23:51:47 +0530

    Add New Customer page changes made

--- src/app/pages/add-customers/add-customers.component.ts
+++ src/app/pages/add-customers/add-customers.component.ts
@@ -1,11 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-customers',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-customers.component.html',
-  styleUrl: './add-customers.component.scss'
+  styleUrl: './add-customers.component.scss',
 })
 export class AddCustomersComponent {
+  addCustomerForm: FormGroup;
+  defaultPassword: string = 'Default@123'; // Assign default password
 
+  constructor(private fb: FormBuilder) {
+    this.addCustomerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      address: ['', Validators.required],
+      consumerNumber: ['', [Validators.required, Validators.minLength(5)]],
+      phoneNumber: [
+        '',
+        [Validators.required, Validators.pattern(/^[0-9]{10,15}$/)],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      customerType: ['', Validators.required],
+      userId: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ],
+      ],
+    });
+  }
+
+  onSubmit() {
+    if (this.addCustomerForm.valid) {
+      const customerData = {
+        ...this.addCustomerForm.value,
+        password: this.defaultPassword,
+      };
+      console.log('Customer Data:', customerData);
+
+      // Call your service to submit the data
+      // this.customerService.addCustomer(customerData).subscribe(
+      //   response => this.handleSuccess(response),
+      //   error => this.handleError(error)
+      // );
+    }
+  }
+
+  handleSuccess(response: any) {
+    alert(`Customer added successfully!
+      Customer ID: ${response.customerId},
+      Name: ${response.fullName},
+      Email: ${response.email}`);
+    // Navigate to add consumer ID page
+  }
+
+  handleError(error: any) {
+    alert(error.message || 'An error occurred while adding the customer.');
+  }
 }
