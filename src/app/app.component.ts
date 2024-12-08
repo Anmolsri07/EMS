01b3fd2a5ce9f2608f966cfba5b9ba7f45388565
omprakash-singh <omprakashsingh11009@gmail.com>
2024-12-08 14:08:14 +0530

    footer changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {  RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  currentUser = localStorage.getItem('user')
   title = 'EMS';
 }
