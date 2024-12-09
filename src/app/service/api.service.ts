035dab6dc2d2954c7266d39608a5f882caeeea7f
omprakash-singh <omprakashsingh11009@gmail.com>
2024-12-10 00:16:59 +0530

    changes made

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { ICustomers, ILogin } from '../interfaces/users';
+import { IConsumer, ICustomers, ILogin } from '../interfaces/users';
 import { IBill } from '../interfaces/bills';
 import { IComplaint } from '../interfaces/complaint';
 import { Observable } from 'rxjs';
@@ -26,6 +26,11 @@ export class ApiService {
   createNewCustomer(payload: ICustomers) {
     return this.http.post(`${this.baseUrl}/api/customers/register`, payload);
   }
+
+  createNewConsumer(payload: IConsumer) {
+    return this.http.post(`${this.baseUrl}/apiAdmin/addNewConsumer`, payload);
+  }
+
   ///search
   getConsumerByCustomerId(customerId: string) {
     return this.http.get(
@@ -65,6 +70,6 @@ export class ApiService {
   }
 
   getAdminAllBills() {
-    return this.http.get(`${this.baseUrl}/apiAdmin/viewBill`)
+    return this.http.get(`${this.baseUrl}/apiAdmin/viewBill`);
   }
 }
